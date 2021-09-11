9079cde010e34ceff5d915b86010f5d7a8099007
Saul Flores <69769641+saulflores90@users.noreply.github.com>
2021-09-11 16:14:47 -0300

    TP 6

---
+++ LectorFrases.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string frase;
+string palabras;
+
+int main()
+{
+	cout << "Introduzca una frase para procesar: ";
+	getline(cin, frase);
+
+	ofstream out_file;
+	out_file.open("frase.txt", ios::out);
+	if(out_file.is_open())
+	{
+		out_file << frase;
+	}
+	out_file.close();
+
+	ifstream inp_file;
+	inp_file.open("frase.txt", ios::in);
+	if(inp_file.is_open())
+	{
+		while(!inp_file.eof())
+		{
+			inp_file >> palabras;
+			cout << palabras << " -> numero de letras: " << palabras.length() << endl;
+		}
+	}
+	inp_file.close();
+
+	cin.get();
+
+	return 0;
+}
